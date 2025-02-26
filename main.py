3006a9318f6a88a004cf4f733ac801e4432ae1a2
TheFatAndFurious <mrguerrilla@gmail.com>
2025-02-26 23:29:38 +0100

    option to print hidden files

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from pathlib import Path
 from rich.markup import escape
 from rich import print
+from typing_extensions import Annotated
+import typer
 
-def main ():
+def main (
+        hidden: Annotated[bool, typer.Option(help="Include hidden files")] = False,
+):
     path = Path('.')
+    print(f"hidden is {hidden}")
+    # HIDDEN FILES
+    if hidden:
+        for x in path.iterdir():
+            if x.is_dir():
+                print(f"[bold blue]{escape(str(x))}[/bold blue]")
+            else:
+                print(str(x))
+
+    # STANDARD PRINTING
     for x in path.iterdir():
-        if x.is_dir():
-            print(f"[bold]{escape(str(x))}[/bold]")
-        else:
+        if x.is_dir() and not x.name.startswith('.'):
+            print(f"[bold blue]{escape(str(x))}[/bold blue]")
+        elif not x.name.startswith('.'):
             print(str(x))
 
+
+
 if __name__ == '__main__':
-    main()
+    typer.run(main)
 
